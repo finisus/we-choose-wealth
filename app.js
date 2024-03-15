e7216de2695f91c2e27754579ac33a651c153bc9
Finisus <finisvisus@gmail.com>
2024-03-15 20:31:15 +0530

    add volume slider

--- app.js
+++ app.js
@@ -40,6 +40,61 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   /* music widget logic */
+  const musicWidget = document.getElementById('music-widget');
+  const playButton = document.getElementById('play-music-button');
+  const pauseButton = document.getElementById('pause-music-button');
+  const audio = document.getElementById('song');
   
+  let isPlaying = false;
+  
+  musicWidget.addEventListener('click', function() {
+    if (isPlaying) {
+      audio.pause();
+      playButton.style.display = 'block';
+      pauseButton.style.display = 'none';
+    } else {
+      audio.play();
+      playButton.style.display = 'none';
+      pauseButton.style.display = 'block';
+    }
+    isPlaying = !isPlaying;
+  });
+  
+  // handle when song ends
+  audio.onended = function() {
+    playButton.style.display = 'block';
+    pauseButton.style.display = 'none';
+    isPlaying = false;
+  };
 
+  /* website volume logic */
+  const volumeDisplay = document.getElementById('volume-display');
+  const volumeSliderContainer = document.getElementById('volume-slider-container');
+  const volumeSlider = document.getElementById('volume-slider');
+  const volumeCurrent = document.getElementById('volume-current');
+  
+  let isSliderVisible = false;
+
+  // set volume based on slider's initial set volume
+  volumeSlider.value = 44;
+  audio.volume = volumeSlider.value / 100;
+
+  volumeDisplay.addEventListener('click', function() {
+    isSliderVisible = !isSliderVisible;
+    volumeSliderContainer.style.display = isSliderVisible ? 'block' : 'none';
+  });
+
+  //close slider when clicking outside
+  document.addEventListener('click', function(event) {
+    if (isSliderVisible && !volumeSliderContainer.contains(event.target) && !volumeDisplay.contains(event.target)) {
+      volumeSliderContainer.style.display = 'none';
+      isSliderVisible = false;
+    }
+  });
+
+  volumeSlider.addEventListener('input', function() {
+    const newVolume = volumeSlider.value / 100;
+    audio.volume = newVolume;
+    volumeCurrent.textContent = volumeSlider.value + '%';
+  });
 });
